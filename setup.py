253919f8bc62c047c420bef67708b07226a75954
Chris Churas <churas.camera@gmail.com>
2023-04-13 17:09:44 -0700

    replaced sklearn with scikit-learn

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['scipy',
-                'sklearn',
+                'scikit-learn',
                 'pandas',
                 'numpy',
                 'dill']
