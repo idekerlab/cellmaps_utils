25ea6cac57d5cbef3b840a021cf8792450c1f840
Chris Churas <churas.camera@gmail.com>
2023-05-05 14:15:26 -0700

    added fairscape-cli as a dependency

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['scipy',
+requirements = ['fairscape-cli',
+                'scipy',
                 'scikit-learn',
                 'pandas',
                 'numpy',
