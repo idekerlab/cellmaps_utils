e1fb8c8fa39eb2b0999909e262a5be0afe7cef82
Chris Churas <churas.camera@gmail.com>
2023-05-18 13:34:46 -0700

    bumped fairscape-cli to 0.1.5a2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['fairscape-cli==0.1.5a1',
+requirements = ['fairscape-cli==0.1.5a2',
                 'scipy',
                 'scikit-learn',
                 'pandas',
