f59116efb1f8c1674cbf21a2a8c90f53e7f9e81c
Chris Churas <churas.camera@gmail.com>
2023-08-09 14:10:59 -0700

    bumped version

--- cellmaps_utils/__init__.py
+++ cellmaps_utils/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for cellmaps_utils."""
 
-__author__ = 'Ideker Lab CM4AI team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a16'
+__version__ = '0.1.0a17'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_utils'
-__description__ = 'Contains utilities needed by CM4AI tools'
+__description__ = 'Contains utilities needed by Cell Maps tools'
