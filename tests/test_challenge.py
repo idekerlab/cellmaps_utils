361c1049beef2cfe970fa5fd0af638d03f30d8db
Chris Churas <churas.camera@gmail.com>
2025-04-11 11:45:36 -0700

    updated tests to reflect migration of code

--- tests/test_challenge.py
+++ tests/test_challenge.py
@@ -8,6 +8,9 @@ import pandas as pd
 
 import cellmaps_utils
 from cellmaps_utils.challenge import *
+from cellmaps_utils.challenge import _get_dataframe
+from cellmaps_utils.challenge import _get_set_of_all_values_of_column_from_dataframes
+from cellmaps_utils.challenge import _generate_mapping
 from cellmaps_utils.crisprtool import CellMapsError
 
 
@@ -22,22 +25,22 @@ class TestChallengeModule(unittest.TestCase):
 
     def test_get_set_of_all_values_of_column_from_dataframes_no_dataframe(self):
         try:
-            get_set_of_all_values_of_column_from_dataframes(None, col_name='xx')
+            _get_set_of_all_values_of_column_from_dataframes(None, col_name='xx')
         except CellMapsError as e:
             self.assertTrue('dataframes cannot be None' in str(e))
 
     def test_get_set_of_all_values_of_column_from_dataframes_no_col_name(self):
         df = pd.DataFrame({'col1': ['val']})
         try:
-            get_set_of_all_values_of_column_from_dataframes([df], col_name=None)
+            _get_set_of_all_values_of_column_from_dataframes([df], col_name=None)
         except CellMapsError as e:
             self.assertTrue('Column name' in str(e))
 
 
     def test_get_set_of_all_values_of_column_from_dataframes_not_list(self):
         df = pd.DataFrame(data={'col1': ['val']})
-        res = get_set_of_all_values_of_column_from_dataframes(dataframes=df,
-                                                              col_name='col1')
+        res = _get_set_of_all_values_of_column_from_dataframes(dataframes=df,
+                                                               col_name='col1')
         self.assertEqual({'val'}, res)
 
     def test_get_dataframe_and_write_tsv_file(self):
@@ -47,7 +50,7 @@ class TestChallengeModule(unittest.TestCase):
             df = pd.DataFrame({'col1': ['val1'], 'col2': ['val2']})
             write_tsv_file(df=df, outfile=dest_file)
 
-            check_df = get_dataframe(dest_file)
+            check_df = _get_dataframe(dest_file)
             self.assertTrue(len(check_df.columns) == 2)
             self.assertTrue('col1' in check_df.columns)
             self.assertTrue('col2' in check_df.columns)
@@ -56,34 +59,24 @@ class TestChallengeModule(unittest.TestCase):
         finally:
             shutil.rmtree(temp_dir)
 
-    def test_write_mapping_file(self):
-        temp_dir = tempfile.mkdtemp()
-        try:
-            dest_file = os.path.join(temp_dir, REPL_MAPPING)
-            write_id_mapping_file(temp_dir, id_mapping={'hi': 'there'})
-            with open(dest_file, 'r') as f:
-                data = json.load(f)
-            self.assertTrue(data['hi'] == 'there')
-        finally:
-            shutil.rmtree(temp_dir)
 
     def test_get_set_of_all_values_of_column_from_dataframes(self):
         df1 = pd.DataFrame(data={'col1': ['1', '2'], 'col2': ['3', '4']})
         df2 = pd.DataFrame(data={'col1': ['5', '2'], 'col2': ['6', '8']})
-        res = get_set_of_all_values_of_column_from_dataframes([df1, df2],
-                                                              col_name='col1')
+        res = _get_set_of_all_values_of_column_from_dataframes([df1, df2],
+                                                               col_name='col1')
         self.assertEqual({'1', '2', '5'}, res)
 
-        res = get_set_of_all_values_of_column_from_dataframes([df1, df2],
-                                                              col_name='col2')
+        res = _get_set_of_all_values_of_column_from_dataframes([df1, df2],
+                                                               col_name='col2')
         self.assertEqual({'3', '4', '6', '8'}, res)
 
     def test_generate_mapping(self):
         cvals = {'a', 'b', 'c', 'd'}
 
-        res = generate_mapping(col_vals=cvals, num_chars=6, mapping_col_name='foo',
-                               input_repl_one='first',
-                               input_repl_two='second')
+        res = _generate_mapping(col_vals=cvals, num_chars=6, mapping_col_name='foo',
+                                input_repl_one='first',
+                                input_repl_two='second')
         self.assertTrue('forward' in res)
         self.assertTrue('reverse' in res)
         self.assertTrue(len(res['forward']) == 4)
@@ -149,6 +142,30 @@ class TestChallengeModule(unittest.TestCase):
         self.assertTrue(res[1] == 2)
         self.assertTrue(res[2] == 1)
         self.assertTrue(res[3] == 1)
+
+    def test_merge_replicate_dataframes(self):
+        repl1 = pd.DataFrame({'xxx': [1, 2, 3],
+                              'r1a': [4, 5, 6],
+                              'r1b': [7, 8, 9]})
+        repl2 = pd.DataFrame({'xxx': [1, 2, 3],
+                              'r2a': [10, 11, 12],
+                              'r2b': [13, 14, 15]})
+        df = merge_replicate_dataframes(repl1_df=repl1,
+                                        repl2_df=repl2)
+
+        self.assertEqual(len(df.columns), 5)
+        self.assertEqual(list(df.columns), ['xxx', 'repl1_r1a', 'repl1_r1b',
+                         'repl2_r2a', 'repl2_r2b'])
+        self.assertEqual(list(df['xxx'].values),
+                         list(repl1['xxx'].values))
+        for orig in ['r1a', 'r1b']:
+            colname = 'repl1_' + orig
+            self.assertEqual(list(df[colname].values),
+                             list(repl1[orig].values), 'repl1: ' + str(repl1.head()))
+        for orig in ['r2a', 'r2b']:
+            colname = 'repl2_' + orig
+            self.assertEqual(list(df[colname].values),
+                             list(repl2[orig].values), 'repl2: ' + str(repl2.head()))
 
 
 if __name__ == '__main__':
