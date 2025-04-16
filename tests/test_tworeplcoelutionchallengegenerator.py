d3863a60a16be1ff1054f9bf8079383e4d4fcec9
Chris Churas <churas.camera@gmail.com>
2025-04-15 18:01:14 -0700

    started full run test

--- tests/test_tworeplcoelutionchallengegenerator.py
+++ tests/test_tworeplcoelutionchallengegenerator.py
@@ -4,9 +4,11 @@ import os
 import shutil
 import tempfile
 import unittest
+import pandas as pd
 from unittest.mock import MagicMock
 import argparse
 from cellmaps_utils.challenge import *
+import cellmaps_utils
 
 
 class TestTwoReplCoelutionChallengeGenerator(unittest.TestCase):
@@ -42,6 +44,48 @@ class TestTwoReplCoelutionChallengeGenerator(unittest.TestCase):
             with open(dest_file, 'r') as f:
                 data = json.load(f)
             self.assertTrue(data['hi'] == 'there')
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_full_run_on_example_repls(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            testdatadir = os.path.join(os.path.dirname(__file__), 'data')
+            repl1_file = os.path.join(testdatadir, 'repl1_example.tsv')
+            repl2_file = os.path.join(testdatadir, 'repl2_example.tsv')
+
+            theargs = self._parser.parse_args([TwoReplCoelutionChallengeGenerator.COMMAND,
+                                               temp_dir,
+                                               '--repl1_tsv', repl1_file,
+                                               '--repl2_tsv', repl2_file,
+                                               '--seed', '1'])
+
+            gen = TwoReplCoelutionChallengeGenerator(theargs)
+            self.assertEqual(0, gen.run())
+            result_dir = os.path.join(temp_dir,
+                                      'tworeplcoelution_challenge_kolf'
+                                      '2.1j_undifferentiated_untreated')
+            self.assertTrue(os.path.isdir(result_dir))
+            mapping_file = os.path.join(result_dir, 'repl1_repl2_id_mapping.json')
+            with open(mapping_file, 'r') as f:
+                data = json.load(f)
+            self.assertTrue(os.path.isfile(os.path.join(result_dir,
+                                                        data['repl1.tsv'])))
+            self.assertTrue(os.path.isfile(os.path.join(result_dir,
+                                                        data['repl2.tsv'])))
+            self.assertTrue('forward' in data)
+            self.assertTrue('reverse' in data)
+            self.assertTrue(data['forward']['P04637'] in data['reverse'])
+            self.assertTrue(data['forward']['A6NCE7;Q9GZQ8'] in data['reverse'])
+            self.assertEqual('P04637', data['reverse'][data['forward']['P04637']])
+            self.assertTrue('A6NCE7;Q9GZQ8' in data['reverse'][data['forward']['A6NCE7;Q9GZQ8']])
+
+            df = pd.read_table(os.path.join(result_dir, 'repl1_repl2_combined.tsv'))
+            self.assertEqual(2, len(df))
+            self.assertTrue(list(df.columns), ['xxx', 'repl1_1',
+                                               'repl1_2', 'repl1_3', 'repl2_1',
+                                               'repl2_2', 'repl2_3'])
+            # self.assertEqual(list(df['xxx']), ['YXHLNEYOI', 'Z6VDIUIGI'])
         finally:
             shutil.rmtree(temp_dir)
 
